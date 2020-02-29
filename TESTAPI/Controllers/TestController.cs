55bb9c2290db28a3c6b602b76d37d04c5d2e13e0
Kasunjith-Bimal <kasunysoft@gmail.com>
2020-03-01 00:28:34 +0530

    Swashbuckle Swagger Configuration

---
+++ TESTAPI/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESTAPI.Controllers
+{
+    public class TestController:Controller
+    {
+        [HttpGet("api/ok")]
+        public IActionResult Get()
+        {
+            return Ok(new { name ="kasun"});
+        }
+    }
+}
---
+++ TESTAPI/Options/SwaggerOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESTAPI.Options
+{
+    public class SwaggerOptions
+    {
+
+        public string JsonRute { get; set; }
+
+        public string Description { get; set; }
+
+        public string UIEndPoint { get; set; }
+ 
+    }
+}
