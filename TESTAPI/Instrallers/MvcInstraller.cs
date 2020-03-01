a6173a33c5e3422fe85c8facadfb483ed0336ad2
Kasunjith-Bimal <kasunysoft@gmail.com>
2020-03-01 18:52:59 +0530

    clearn statup

---
+++ TESTAPI/Instrallers/MvcInstraller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TESTAPI.Instrallers
+{
+    public class MvcInstraller : IInstraller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API", Version = "V1" });
+            });
+        }
+    }
+}
