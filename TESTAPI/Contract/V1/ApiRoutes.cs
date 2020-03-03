b38ba686934fc7047ca9e0b74596118e4d5fc2f2
Kasunjith-Bimal <kasunysoft@gmail.com>
2020-03-04 01:12:57 +0530

    post reqest

--- TESTAPI/Contract/V1/ApiRoutes.cs
+++ TESTAPI/Contract/V1/ApiRoutes.cs
@@ -17,6 +17,10 @@ namespace TESTAPI.Contract.V1
         public static class Posts
         {
             public const string GetAll = Base + "/posts";
+
+            public const string Create = Base + "/posts";
+
+            public const string Get = Base + "/posts/{postId}";
         }
 
     }
---
+++ TESTAPI/Controllers/V1/Responses/CreatePostResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESTAPI.Controllers.V1.Responses
+{
+    public class CreatePostResponse
+    {
+        public string Id { get; set; }
+    }
+}
