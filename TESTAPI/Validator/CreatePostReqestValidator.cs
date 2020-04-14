520b3ecf7d2ebc48bf429190c9d2aa07ba676a2d
Kasunjith-Bimal <kasunysoft@gmail.com>
2020-04-14 14:13:19 +0530

    add fluent validation using asp .net core

---
+++ TESTAPI/Validator/CreatePostReqestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TESTAPI.Contract.V1.Requests;
+
+namespace TESTAPI.Validator
+{
+    public class CreatePostReqestValidator:AbstractValidator<CreatePostReqest>
+    {
+        public CreatePostReqestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Matches("^[a-zA-Z0-9 ]*$");
+        }
+    }
+}
