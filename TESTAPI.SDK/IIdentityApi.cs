6d863d89a30f7f6eda20551d6df79cd429494f78
Kasunjith-Bimal <kasunysoft@gmail.com>
2020-04-19 01:44:02 +0530

    Create SDK using Refit

---
+++ TESTAPI.SDK/IIdentityApi.cs
@@ -0,0 +1,25 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TESTAPI.Contract.V1.Requests;
+using TESTAPI.Contract.V1.Responses;
+
+namespace TESTAPI.SDK
+{
+    public interface IIdentityApi
+    {
+        [Post("/api/v1/identity/register")]
+        Task<ApiResponse<AuthSucessResponse>> RegisterAsync([Body] UserRegistrationReqest registrationReqest);
+
+        [Post("/api/v1/identity/login")]
+        Task<ApiResponse<AuthSucessResponse>> LoginAsync([Body] UserLoginReqest loginReqest);
+
+        [Post("/api/v1/identity/Refresh")]
+        Task<ApiResponse<AuthSucessResponse>> RefreshAsync([Body] RefreshTokenReqest refreshTokenReqest);
+
+
+
+    }
+}
---
+++ TESTAPI.SDK/IPostApi.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TESTAPI.Contract.V1.Requests;
+using TESTAPI.Contract.V1.Responses;
+
+namespace TESTAPI.SDK
+{
+   public interface IPostApi
+    {
+        [Get("/api/v1/posts")]
+        Task<ApiResponse<List<PostResponse>>> GetAllAsync();
+
+        [Get("/api/v1/posts/{postId}")]
+        Task<ApiResponse<PostResponse>> GetAsync(Guid postId);
+
+        [Post("/api/v1/posts")]
+        Task<ApiResponse<PostResponse>> CreateAsync([Body] CreatePostReqest createPostReqest);
+
+
+        [Put("/api/v1/posts/{postId}")]
+        Task<ApiResponse<PostResponse>> UpdateAsync(Guid postId,[Body] UpdatePostReqest updatePostReqest);
+
+        [Delete("/api/v1/posts/{postId}")]
+        Task<ApiResponse<string>> DeleteAsync(Guid postId);
+         
+    }
+}
